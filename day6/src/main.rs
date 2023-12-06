b0b1e37f525b3246a6aa82163d03995d43050997
davenpcj <davenpcj5542009@yahoo.com>
2023-12-06 17:28:56 -0600

    Day6 initial completion

---
+++ day6/src/main.rs
@@ -0,0 +1,89 @@
+use std::io::{Lines, BufRead, BufReader};
+
+use anyhow::{Context,Error};
+
+const DEBUG:bool = cfg!(debug_assertions);
+
+fn str_to_vec(somestr: &str, skip:usize) -> Vec<u32> {
+    somestr.split_ascii_whitespace().skip(skip).map(|s| u32::from_str_radix(s, 10).expect("string to be sequence of u32")).collect()
+}
+
+const ACCEL:u32 = 1; // 1 ms-per-ms
+
+fn distance(hold_time: u32, race_time: u32) -> u32 {
+    if hold_time >= race_time {
+        return 0;
+    }
+
+    return (race_time-hold_time)*(hold_time);
+}
+
+fn ways_to_win(racetime: u32, record: u32) -> u32 {
+    let mut ways_to_win = 0;
+    for t in 1..racetime {
+        if distance(t, racetime) > record {
+            ways_to_win += 1;
+        }
+    }
+    if DEBUG { eprintln!("race wins: {:?}", &ways_to_win) };
+    return ways_to_win;
+}
+
+fn go(input:&mut dyn BufRead) -> Result<(),Error>{
+    // boat racing
+    //Time:      7  15  30
+    //Distance:  9  40 200
+
+    // first, second, third races
+    // whole time for race, then record distance for races
+    
+    let mut lines = BufReader::new(input).lines();
+
+    let times_str = lines.next().unwrap().unwrap();
+    let dist_str = lines.next().unwrap().unwrap();
+
+    let times = str_to_vec(&times_str, 1);
+    let records = str_to_vec(&dist_str, 1);
+
+    if DEBUG { eprintln!("times: {:?}", &times_str) };
+
+    
+    let mut margin_error = 1;
+    for race in 0..times.len() {
+        margin_error *= ways_to_win(times[race], records[race]);
+    }
+    
+    if DEBUG { eprintln!("margin_error: {:?}", &margin_error) };
+
+    return Ok(());
+}
+
+fn main() -> Result<(),Error> {
+    go(&mut std::io::stdin().lock())
+}
+
+#[test]
+fn example() -> Result<(),Error> {
+    let testinput = 
+r"Time:      7  15   30
+Distance:  9  40  200";
+
+    go(&mut testinput.as_bytes())
+}
+
+#[test]
+fn testinput1() {
+    let testinput = "Time:      7  15  30\n";
+    assert_eq!(str_to_vec(&testinput,1), [7,15,30]);
+}
+
+#[test]
+fn test() {
+    assert_eq!(distance(1, 7), 6);
+    assert_eq!(distance(2, 7), 10);
+    assert_eq!(distance(3, 7), 12);
+    assert_eq!(distance(4, 7), 12);
+    assert_eq!(distance(5, 7), 10);
+    assert_eq!(distance(6, 7), 6);
+}
+
