e918f3f998aeb0d5c4db127184eb123bc72756fe
davenportc <chris.davenport@hpe.com>
2023-12-07 17:11:02 -0600

    Day5 part 2, complete and fast using range translation method.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -33,6 +33,11 @@ fn str_to_vec(somestr: &str, skip:usize) -> Vec<u32> {
     somestr.split_ascii_whitespace().skip(skip).map(|s| s.parse().expect("string to be sequence of u32")).collect()
 }
 
+fn map_location(value: u32, input: &RangeInclusive<u32>, output: &RangeInclusive<u32>) -> u32 {
+    if DEBUG { eprint!("{{{value}-{}+{}}}", input.start(), output.start())}
+    value - input.start() + output.start()
+}
+
 fn convert_location(seed: u32, maps: &Vec<&RangeInclusiveMap<u32,RangeInclusive<u32>>>) -> u32 {
     let mut dest = seed;
     if DEBUG { eprint!("{seed}") };
@@ -40,8 +45,7 @@ fn convert_location(seed: u32, maps: &Vec<&RangeInclusiveMap<u32,RangeInclusive<
         dest = match map.get_key_value(&dest) {
             // 79, given 50..(50+48) => 52, should be 81
             Some((input,output)) => { 
-                if DEBUG { eprint!("{{{dest}-{}+{}}}", input.start(), output.start())}
-                dest - input.start() + output.start() 
+                map_location(dest, input, output)
             },
             None => dest,
         };
@@ -49,6 +53,109 @@ fn convert_location(seed: u32, maps: &Vec<&RangeInclusiveMap<u32,RangeInclusive<
     }
     if DEBUG { eprintln!("; {seed} => {dest}") };
     return dest;
+}
+// take input range, return range of input which is left of target, if any, and range of input which is right of target, if any
+fn trim_range(input: &RangeInclusive<u32>, target: &RangeInclusive<u32>) -> (Option<RangeInclusive<u32>>, Option<RangeInclusive<u32>>) {
+    let mut left = None;
+    let right ;
+    if input.start() < target.start() {
+        left = Some(
+            if input.end() < target.start() {
+                // non overlapping
+                input.clone()
+            } else {
+                RangeInclusive::new(*input.start(), *target.start()-1)
+            }
+        );
+    } 
+    
+    if input.start() <= target.end() {
+        right = 
+            if input.end() <= target.end() {
+                // fully overlapping
+                None
+            } else {
+                Some(RangeInclusive::new(*target.end()+1, *input.end()))
+            }
+        ;
+    } else {
+        // non overlapping
+        right = Some(input.clone());
+    }
+
+    if DEBUG { eprint!(" => Trim({left:?},{right:?})")}
+    
+    (left, right)
+}
+
+trait RangeMapper {
+    fn map(self, value: u32) -> u32;
+}
+
+impl RangeMapper for (&RangeInclusive<u32>,&RangeInclusive<u32>) {
+    fn map(self, value: u32) -> u32 {
+        map_location(value, self.0, self.1)
+    }
+}
+
+fn convert_location_range(seed: RangeInclusive<u32>, maps: &Vec<&RangeInclusiveMap<u32,RangeInclusive<u32>>>) -> Vec<RangeInclusive<u32>> {
+    let mut dest = VecDeque::from([seed.clone()]);
+    if DEBUG { eprint!("{seed:?}") };
+    for &map in maps {
+        let mut newdest = Vec::with_capacity(dest.len());
+        while let Some(dest_r) = dest.pop_front() {
+            if !map.overlaps(&dest_r) {
+                newdest.push(dest_r);
+                continue;
+            }
+
+            for (input,output) in map.overlapping(&dest_r) {
+                if DEBUG { eprint!(" => matching {input:?}")}
+                // 79, given 50..(50+48) => 52, should be 81
+                match trim_range(&dest_r, &input) {
+                    // entirely overlaps the mapped range
+                    // |-------------dest_r------------------|
+                    // |----left---|----input----|---right---|
+                    (Some(left), Some(right)) => {
+                        // convert the known area.
+                        newdest.push(output.clone());
+                        // save the remainder to convert elsewhere or use
+                        dest.push_back(left);
+                        dest.push_back(right);
+                    },
+                    // extends to the left of the mapped range
+                    // |-------------dest_r--------------|
+                    // |----left---|----input----------------|
+                    (Some(left), None) => {
+                        // translate overlap area to output
+                        newdest.push(*output.start()..=map_location(*dest_r.end(), &input, output));
+                        // save the remainder to convert elsewhere or use
+                        dest.push_back(left);
+                    },
+                    // extends to the right of the mapped range
+                    //     |---------dest_r------------------|
+                    // |----------------input----|---right---|
+                    (None, Some(right)) => {
+                        // translate overlap area to output
+                        newdest.push(map_location(*dest_r.start(), &input, output)..=*output.end());
+                        // save the remainder to convert elsewhere or use
+                        dest.push_back(right);
+                    },
+                    // entirely within mapped range
+                    //   |-------------dest_r---|
+                    // |----------------input---------------|
+                    (None,None) => {
+                        // translate directly to output
+                        newdest.push(RangeInclusive::new(map_location(*dest_r.start(), &input, output), map_location(*dest_r.end(), &input, output)));
+                    }
+                }
+            }
+        }
+        dest = newdest.into();
+        if DEBUG { eprintln!(" => {dest:?}") };
+    }
+    if DEBUG { eprintln!("; {seed:?} => {dest:?}") };
+    return dest.into();
 }
 
 fn go(input:&mut dyn BufRead) -> Result<(),Error>{
@@ -107,8 +214,9 @@ fn go(input:&mut dyn BufRead) -> Result<(),Error>{
     println!("{min_loc}");
 
     // PART TWO. The seed numbers are ranges, with the start and length in each pair
-    let min_loc = seedlist.chunks(2).map(|chunk|RangeInclusive::new(chunk[0], chunk[0]+(chunk[1]-1)).into_iter()).flatten()
-        .map(|seed|convert_location(seed, &deque)).min().expect("empty seedlist");
+    eprintln!("PART TWO");
+    let min_loc = seedlist.chunks(2).map(|chunk|RangeInclusive::new(chunk[0], chunk[0]+(chunk[1]-1)))
+        .map(|seed|convert_location_range(seed, &deque)).flatten().map(|locrange|*locrange.start()).min().expect("empty seedlist");
     println!("{min_loc}");
 
     return Ok(());
@@ -216,21 +324,35 @@ fn test4() {
     let mut v = Vec::new();
 
     let mut rmap: RangeInclusiveMap<u32,RangeInclusive<u32>> = RangeInclusiveMap::new();
-    rmap.insert(50..=52, 76..=79);
+    rmap.insert(50..=52, 75..=78);
     rmap.insert(52..=100,50..=98);
     v.push(&rmap);
 
     let mut rmap: RangeInclusiveMap<u32,RangeInclusive<u32>> = RangeInclusiveMap::new();
-    rmap.insert(75..=77, 98..=100);
+    rmap.insert(76..=78, 98..=100);
     rmap.insert(102..=150,50..=98);
     v.push(&rmap);
 
     assert_eq!(convert_location(1, &v), 1);
-    assert_eq!(convert_location(50, &v), 99);
-    assert_eq!(convert_location(51, &v), 77);
+    assert_eq!(convert_location(50, &v), 75);
+    assert_eq!(convert_location(51, &v), 98);
     assert_eq!(convert_location(52, &v), 50);
     assert_eq!(convert_location(53, &v), 51);
     assert_eq!(convert_location(101, &v), 101);
+}
+
+#[test]
+fn test5() {
+    let r = 32..=64;
+    let lefttarget = 20..=30;
+    let righttarget = 65..=100;
+    let containedtarget = 40..=50;
+    let largetarget = 16..=80;
+
+    assert_eq!(trim_range(&r, &lefttarget), (None,Some(r.clone())));
+    assert_eq!(trim_range(&r, &righttarget), (Some(r.clone()), None));
+    assert_eq!(trim_range(&r, &containedtarget), (Some(32..=39),Some(51..=64)));
+    assert_eq!(trim_range(&r, &largetarget), (None,None));
 }
 
 //let mut h = HashMap::new();
