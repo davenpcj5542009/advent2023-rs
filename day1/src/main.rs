35d26fdf5bbe4a8f9462921259a4eaf8fc4c811d
davenpcj <davenpcj5542009@yahoo.com>
2023-12-08 20:46:28 -0600

    Day 1 part 2 complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -18,6 +18,51 @@ fn get_calibration(calib_str:&String) -> u32 {
     return calib;
 }
 
+const WORDVALUES:[&str;10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+fn word_to_value(value_str:&str) -> u32 {
+    if let Ok(val) = value_str.parse() {
+        return val;
+    }
+    WORDVALUES.iter().position(|&v| v == value_str).unwrap() as u32
+}
+
+fn get_calibration_words(calib_str:&String) -> u32 {
+    // in part 2, spelled words can form digits
+    let mut first = 0;
+
+    'strloop:
+    for pos in 0..calib_str.len() {
+        for word in 0..WORDVALUES.len() {
+            if calib_str.chars().nth(pos).unwrap().is_ascii_digit() {
+                first = calib_str.chars().nth(pos).unwrap().to_digit(10).unwrap();
+                break 'strloop;
+            }
+            if calib_str.get(pos..).unwrap().starts_with(&WORDVALUES[word]) {
+                first = word as u32;
+                break 'strloop;
+            }
+        }
+    }
+
+    let mut last = 0;
+    'strloop:
+    for pos in (0..calib_str.len()).rev() {
+        for word in 0..WORDVALUES.len() {
+            if calib_str.chars().nth(pos).unwrap().is_ascii_digit() {
+                last = calib_str.chars().nth(pos).unwrap().to_digit(10).unwrap();
+                break 'strloop;
+            }
+            if calib_str.get(pos..).unwrap().starts_with(&WORDVALUES[word]) {
+                last = word as u32;
+                break 'strloop;
+            }
+        }
+    }
+
+    return first*10+last;
+}
+
 fn go(input:&mut dyn BufRead) -> Result<(),Error>{
     // trebuchet calibration
     // 1abc2
@@ -26,10 +71,10 @@ fn go(input:&mut dyn BufRead) -> Result<(),Error>{
     let mut lines = BufReader::new(input).lines();
 
     let mut calib_sum = 0;
+    println!("PART TWO");
 
     while let Some(Ok(calib_str)) = lines.next() {
-
-        let calib:u32 = get_calibration(&calib_str);
+        let calib:u32 = get_calibration_words(&calib_str);
         if DEBUG { eprintln!("{calib_str} => {calib}") };
         calib_sum += calib;
     }
@@ -50,6 +95,20 @@ r"1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet";
+
+    go(&mut testinput.as_bytes())
+}
+
+#[test]
+fn example2() -> Result<(),Error> {
+    let testinput = 
+r"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
 
     go(&mut testinput.as_bytes())
 }
