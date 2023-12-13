ce5d540e6c0a9a8824a76716b2c2a0c54a9fa556
davenpcj <davenpcj5542009@yahoo.com>
2023-12-12 22:49:15 -0600

    Day 11 Part 1 complete, part 2 implementation

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -41,6 +41,36 @@ fn expand_universe(grid: &mut Vec<Vec<char>>) {
     }
 }
 
+fn expand_old_universe(grid: &Vec<Vec<char>>) -> (Vec<usize>,Vec<usize>) {
+    let mut expanded_rows = Vec::new();
+    let mut expanded_cols = Vec::new();
+    let mut row = 0;
+    let mut col = 0;
+    // if row empty, insert new row above
+    while row < grid.len() {
+        if grid[row].iter().all(|&c|c == '.') {
+            if DEBUG { eprintln!("Expanding row {row}") };
+            expanded_rows.push(row);
+        }
+        row += 1;
+    }
+
+    // if column empty, insert new column left
+    while col < grid[0].len() {
+        if grid.iter().map(|rv|rv[col]).all(|c|c=='.') {
+            if DEBUG { eprintln!("Expanding col {col}") };
+            expanded_cols.push(col);
+        }
+        col += 1;
+    }
+
+    let expansions = (expanded_rows, expanded_cols);
+
+    if DEBUG { eprintln!("EXPANSIONS: {:?}", &expansions) };
+
+    return expansions;
+}
+
 fn compute_distances(grid:&Vec<Vec<char>>) -> Vec<usize> {
     let gxy: Vec<(usize,usize)> = grid.iter().enumerate().map(
         |(r_idx, row)| row.iter().enumerate().filter_map(
@@ -63,6 +93,30 @@ fn compute_distances(grid:&Vec<Vec<char>>) -> Vec<usize> {
     return dist;
 }
 
+fn compute_distances_expanded(grid:&Vec<Vec<char>>, factor:usize, expansions:&(Vec<usize>,Vec<usize>)) -> Vec<usize> {
+    let gxy: Vec<(usize,usize)> = grid.iter().enumerate().map(
+        |(r_idx, row)| row.iter().enumerate().filter_map(
+            move |(c_idx, c)|(*c == '#').then(||(r_idx,c_idx))
+        )
+    ).flatten().collect();
+
+    if DEBUG { eprintln!("FOUND GALAXIES: [{gxy:?}]") };
+
+    let mut dist = Vec::new();
+    // loop through every pair
+    for (n,g) in gxy.iter().enumerate() {
+        for g2 in gxy.iter().skip(n+1) {
+            let mut gdist = g.0.abs_diff(g2.0) + g.1.abs_diff(g2.1); 
+            gdist += (factor-1)*expansions.0.iter().filter(|&&x|x < usize::max(g.0, g2.0) && x > usize::min(g.0,g2.0)).count();
+            gdist += (factor-1)*expansions.1.iter().filter(|&&x|x < usize::max(g.1, g2.1) && x > usize::min(g.1,g2.1)).count();
+            if DEBUG { eprintln!("{g:?} -> {g2:?}: {gdist:?}") };
+            dist.push(gdist);
+        }
+    }
+    
+    return dist;
+}
+
 fn go(input:&mut dyn BufRead) -> Result<(),Error>{
     // galactic observatory
     // puzzle input, star map of galaxies
@@ -73,12 +127,12 @@ fn go(input:&mut dyn BufRead) -> Result<(),Error>{
     if DEBUG { eprintln!("grid: {:?}", Grid::from(&grid)) };
 
     // expand the universe
-    expand_universe(&mut grid);
+    let expansions = expand_old_universe(&mut grid);
 
     if DEBUG { eprintln!("expanded: {:?}", Grid::from(&grid)) };
 
     // compute the distances
-    let dist = compute_distances(&grid);
+    let dist = compute_distances_expanded(&grid, 1_000_000, &expansions);
 
     if DEBUG { eprintln!("dist: {:?}", &dist) };
 
@@ -86,7 +140,7 @@ fn go(input:&mut dyn BufRead) -> Result<(),Error>{
     let steps:usize = dist.iter().sum();
 
     // PART TWO
-    // eprintln!("PART TWO");
+    eprintln!("PART TWO");
 
     // follow the map steps
 
@@ -197,4 +251,47 @@ r"....#........
     let check_dist = compute_distances(&grid);
 
     eprintln!("{:?}", &check_dist);
+}
+
+#[test]
+fn testinput3() {
+    let testinput = 
+r"...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+    
+    let grid = testinput.split_ascii_whitespace().map(|line|line.chars().collect::<Vec<char>>()).collect::<Vec<_>>();
+
+    let expansions = expand_old_universe(&grid);
+
+    let check_dist = compute_distances_expanded(&grid, 1, &expansions);
+
+    eprintln!("Factor 1 {:?}", &check_dist);
+
+    let steps:usize = check_dist.iter().sum();
+
+    eprintln!("{steps}");
+
+    let check_dist = compute_distances_expanded(&grid, 10, &expansions);
+
+    eprintln!("Factor 10 {:?}", &check_dist);
+
+    let steps:usize = check_dist.iter().sum();
+
+    eprintln!("{steps}");
+
+    let check_dist = compute_distances_expanded(&grid, 100, &expansions);
+
+    eprintln!("Factor 100 {:?}", &check_dist);
+
+    let steps:usize = check_dist.iter().sum();
+
+    eprintln!("{steps}");
 }
